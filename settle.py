9c36045bec0381e76f9b012fab52b39c4c66d8a2
Peter Owen <petarggh@gmail.com>
2020-10-10 23:47:09 -0700

    added year folder

--- settle.py
+++ settle.py
@@ -1,44 +1,88 @@
 import os
 import sys
+import shutil
+import errno
+from PIL import Image
+from PIL.ExifTags import TAGS
 
-directory = '.'
 
-if len(sys.argv) > 1:
-    directory = sys.argv[1]
+def getFileType(filename):
+    filetype = os.path.splitext(filename)[1].lower()
+    if(filetype == ".jpeg"):
+        filetype = ".jpg"
+    print(filetype)
+    return filetype
 
-for subdir, dirs, files in os.walk(directory):
+
+def renameJpg(filePath, fileName):
+    dateTime = "000000"
+    exif = {}
+    ext = os.path.splitext(fileName)[1]
+    try:
+        image = Image.open(filePath)
+        image.verify()
+        for (key, value) in image._getexif().items():
+            exif[TAGS.get(key)] = value
+        if(exif.get("DateTime")):
+            dateTime = str(exif.get("DateTime")).replace(":", "")
+        elif(exif.get("DateTimeOriginal")):
+            dateTime = str(exif.get("DateTimeOriginal")).replace(":", "")
+        elif(exif.get("DateTimeDigitized")):
+            dateTime = str(exif.get("DateTimeDigitized")).replace(":", "")
+        elif(exif.get("29")):
+            dateTime = str(exif.get("29")).replace(":", "")
+    except Exception:
+        dateTime = "000000"
+    dateTime = str(dateTime).replace(" ", "-", 1)
+    newName = dateTime + "__" + fileName
+    newName = str(newName).replace(" ", "_")
+    if ext != "jpg":
+        newName = newName.replace(ext, getFileType(newName))
+    return newName
+
+
+def getYear(fileName):
+    year = newName[0:4]
+    return year
+
+
+def doTheCopy(fileType, fullPath, newName):
+    finalPath = outbox+'/'+fileType+'/'+getYear(newName)+'/'
+    try:
+        shutil.copy(fullPath, finalPath+newName)
+    except IOError as e:
+        if e.errno != errno.ENOENT:
+            raise
+        # try creating parent directories
+        os.makedirs(os.path.dirname(finalPath))
+        shutil.copy(fullPath, finalPath+newName)
+
+
+inbox = '../TestInbox'
+outbox = '../TestOutbox'
+
+
+if len(sys.argv) == 3:
+    inboxDirectory = sys.argv[1]
+    outboxDirectory = sys.argv[2]
+
+for subdir, dirs, files in os.walk(inbox):
     for file in files:
-        print(os.path.join(subdir, file))
-        # check file Type
-        # if image
-        #   read meta to determine create date
-        #   if exists
-        #       prefix filename with datetime string
-        #       move to folder for that year
-        #   if # NOT
-        #       check other date fields
-        #       if a good one
-        #           use that
-        #           move to the folder for that year
-        #       else
-        #           put in the mystery folder
-        # if video
-        #   read meta to determine create date
-        #   if exists
-        #       prefix filename with datetime string
-        #       move to folder for that year
-        #   if # NOT
-        #       check other date fields
-        #       if a good one
-        #           use that
-        #           move to the folder for that year
-        #       else
-        #           put in the mystery folder
-        # if document
-        #   Move to document folder
-        # if software
-        #   move to software folder
-        # if disk image
-        #   move to disk image folder
-        # if archive
-        #   move to archive folder
+        fullPath = os.path.join(subdir, file)
+        type = getFileType(file)
+        newName = "noName"
+        if(type == ".jpg"):
+            newName = renameJpg(fullPath, file)
+        elif(type == ".gif"):
+            print("GIF")
+        elif(type == ".bmp"):
+            print("BMP")
+        elif(type == ".png"):
+            print("PING")
+        elif(type == ".tif"):
+            print("WTIF")
+        else:
+            print("I DON'T KNOW")
+        # newPath = outbox + '/' + type.strip('.') + '/' + newName
+        if(newName != "noName"):
+            doTheCopy(type.strip('.'), fullPath, newName)
