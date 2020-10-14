70b1ead461008e8073a141c79b9853f9067fba55
Peter Owen <petarggh@gmail.com>
2020-10-13 21:14:35 -0700

    added dupe directory handling

--- settle.py
+++ settle.py
@@ -14,13 +14,13 @@ def getFileType(filename):
     return filetype.strip('.')
 
 
-def renameImage(filePath, fileName):
+def renameImage(sourcePath, fileName):
     dateTime = "000000"
     exif = {}
     ext = getFileType(fileName)
     if(ext != "zip" and ext != "7z" and ext != "txt" and ext != "pdf"):
         try:
-            image = Image.open(filePath)
+            image = Image.open(sourcePath)
             image.verify()
             for (key, value) in image._getexif().items():
                 exif[TAGS.get(key)] = value
@@ -48,22 +48,22 @@ def getYear(fileName):
     return year
 
 
-def doTheCopy(fullPath, newName):
+def doTheCopy(sourcePath, finalPath, newName):
     global dupeFiles, outbox, dupeBox
-    finalPath = outbox+'/'+getFileType(newName)+'/'+getYear(newName)+'/'
-    if(os.path.exists(finalPath+newName)):
-        if(filecmp.cmp(fullPath, finalPath+newName)):
+    # finalPath = outbox+'/'+getFileType(newName)+'/'+getYear(newName)+'/'
+    if(os.path.exists(outbox+finalPath+newName)):
+        if(filecmp.cmp(sourcePath, outbox+finalPath+newName)):
             newName = 'dupe_' + newName
-            finalPath = outbox+'/'+dupeBox+'/'+getFileType(newName)+'/'+getYear(newName)+'/'
+            finalPath = dupeBox+'/'+finalPath+'/'
             dupeFiles += 1
     try:
-        shutil.copy2(fullPath, finalPath+newName)
+        shutil.copy2(sourcePath, outbox+finalPath+newName)
     except IOError as e:
         if e.errno != errno.ENOENT:
             raise
         # try creating parent directories
-        os.makedirs(os.path.dirname(finalPath))
-        shutil.copy2(fullPath, finalPath+newName)
+        os.makedirs(os.path.dirname(outbox+finalPath))
+        shutil.copy2(sourcePath, outbox+finalPath+newName)
 
 
 inbox = '../TestInbox'
@@ -74,16 +74,19 @@ filesSkipped = 0
 dupeFiles = 0
 
 if len(sys.argv) == 3:
-    inboxDirectory = sys.argv[1]
-    outboxDirectory = sys.argv[2]
+    inbox = sys.argv[1]
+    outbox = sys.argv[2]
+    dupeBox = sys.argv[3]
 
+outbox = outbox + '/'
 for subdir, dirs, files in os.walk(inbox):
     for file in files:
-        fullPath = os.path.join(subdir, file)
+        sourcePath = os.path.join(subdir, file)
         newName = ""
-        newName = renameImage(fullPath, file)
+        newName = renameImage(sourcePath, file)
         if(newName != ""):
-            doTheCopy(fullPath, newName)
+            finalPath = getFileType(newName)+'/'+getYear(newName)+'/'
+            doTheCopy(sourcePath, finalPath, newName)
             filesProcessed += 1
         else:
             filesSkipped += 1
